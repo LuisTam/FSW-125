160ad6f646601b6fc5590ec5e5667404597c0b1f
Luis.tamayo <luis.tamayo@bryanuniversity.edu>
2023-02-16 12:49:23 -0700

    Week3

--- Week3/routes/nonRecycleableRouter.js
+++ Week3/routes/nonRecycleableRouter.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const nonRecyclableItemsRouter = express.Router();
 const { v4: uuidv4 } = require('uuid');
-
+//list of non recyclable items
 let nonRecyclabes = [
     {name: "Batteries", description: "all kind", recyclable: false, ppu: "10.00", _id: uuidv4()},
     {name: "Tires", description: "rubber", recyclable: false, ppu: "50.00", _id: uuidv4()},
     {name: "Plastic Bag", description: "Plastic", recyclable: false, ppu: ".10", _id: uuidv4()},
     {name: "Appliances", description: "all kind", recyclable: false, ppu: "100.00", _id: uuidv4()}
 ]
-
+//GET http://localhost:3000/recyclo
 nonRecyclableItemsRouter.get('/', (req, res) => {
     res.send(nonRecyclabes);
 })
-
+// new item to POST http://localhost:3000/non-reclyclable
 nonRecyclableItemsRouter.post('/', (req, res) => {
     const newNonRec = req.body;
     newNonRec._id = uuidv4();
     nonRecyclabes.push(newNonRec);
     res.send(`Added ${newNonRec.name} to database!`)
 })
-
+//exporting router
 module.exports = nonRecyclableItemsRouter;
--- Week3/server.js
+++ Week3/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-
+//Here you require both routers
 const recycledItemsRouter = require('./routes/recycledItemsRouter')
 const nonRecyclableItemsRouter = require('./routes/nonRecycleableRouter')
 
 const PORT = 3000;
-
+//Turns JSON into readable JS
 app.use(express.json())
-
+//GET http://localhost:3000/recycled-items
 app.use('/recycled-items', recycledItemsRouter)
-
+//GET http://localhost:3000/non-recyclables
 app.use('/non-recyclables', nonRecyclableItemsRouter)
 
 
